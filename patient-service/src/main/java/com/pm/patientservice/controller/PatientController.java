499756de8e2f67ca443494c9996a29e2b71ce9ec
Thirana Embuldeniya <t.embuldeniya@gmail.com>
2025-04-16 12:36:48 +0530

    added a endpoint to create a new patient

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -1,11 +1,11 @@
 package com.pm.patientservice.controller;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.servicce.PatientService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,9 +20,19 @@ public class PatientController {
     }
 
     @GetMapping
-    public ResponseEntity<List<PatientResponseDTO>> getPatients(){
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
         List<PatientResponseDTO> patients = patientService.getPatients();
         return ResponseEntity.ok().body(patients);
     }
 
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(
+            @Validated
+            @RequestBody PatientRequestDTO patientRequestDTO) {
+
+        PatientResponseDTO patientResponseDTO = patientService.createPatient(
+                patientRequestDTO);
+
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
 }
--- patient-service/src/main/java/com/pm/patientservice/servicce/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/servicce/PatientService.java
@@ -1,5 +1,6 @@
 package com.pm.patientservice.servicce;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
@@ -17,8 +18,18 @@ public class PatientService {
         this.patientRepository = patientRepository;
     }
 
+    // service for get all patients
     public List<PatientResponseDTO> getPatients(){
         List<Patient> patients = patientRepository.findAll();
         return patients.stream().map(PatientMapper::toDTO).toList();
+    }
+
+    //service for create new patient
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        Patient newPatient = patientRepository.save(
+                PatientMapper.toModel(patientRequestDTO)
+        );
+
+        return PatientMapper.toDTO(newPatient);
     }
 }
